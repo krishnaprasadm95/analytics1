c3227f4be2092c2fe2911af256b7397319f902f4
Krishna Prasad M <krishnaprasadm1995@gmail.com>
2018-10-25 14:17:13 +0530

    25

---
+++ intro to R.R
@@ -0,0 +1,21 @@
+x1 = 3
+
+data(women)
+
+?mtcars
+mtcars
+mtcars$mpg
+rm(x1)
+?mean
+x <- c(0:10, 50)
+x
+xm <- mean(x)
+xm
+c(xm, mean(x, trim = 0.10))
+
+seq(0,100,by=2)
+seq(1,100,by=2)
+
+seq(1,10, length.out = 5)
+seq(1,10, length.out = 4)
+plot(1:10)
